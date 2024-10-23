5974a69de78564542bbae372db265aacbaf140d6
Victor Vieira dos Santos <64611868+VictorVdoS@users.noreply.github.com>
2024-10-23 09:46:00 -0300

    Add files via upload

---
+++ Desafio/report_mysql/Consultas_tabela.sql
@@ -0,0 +1,17 @@
+-- Selecionar todos os valores da tabela employee2
+SELECT * FROM employee2;
+
+-- Selecionar todos os valores da tabela dependent
+SELECT * FROM dependent;
+
+-- Selecionar todos os valores da tabela departament
+SELECT * FROM departament;
+
+-- Selecionar todos os valores da tabela dept_locations
+SELECT * FROM dept_locations;
+
+-- Selecionar todos os valores da tabela project
+SELECT * FROM project;
+
+-- Selecionar todos os valores da tabela works_on
+SELECT * FROM works_on;
